61454f0192888f6cd858d24deef8e15d6b295bf9
piisopii <shofiputried45@gmail.com>
2022-10-19 10:48:16 +0700

    modif css dan index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,137 @@
+<!-- Coding by CodingLab | www.codinglabweb.com -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    
+    <!----======== CSS ======== -->
     <link rel="stylesheet" href="style.css">
+    
+    <!----===== Boxicons CSS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+    <title>Dashboard</title> 
 </head>
 <body>
-    <div class="sidebar">
-        <header>Dinas Pertanahan dan Tata Ruang DIY</header>
-        <ul>
-            <li><a href="#"><i class="fas fa-user" style="color: black"></i></a></li>
-            <li><a href="#"><i class="fas fa-home" style="color: black"></i></a></li>
-            <li><a href="#"><i class="fas fa-folder" style="color: black"></i></a></li>
-            <li><a href="#"><i class="fas fa-qrcode" style="color: black"></i></a></li>
-            <li><a href="#"><i class="fas fa-document" style="color: black"></i></a></li>
-            <li><a href="#"><i class="fas fa-qrcode" style="color: black"></i></a></li>
-        </ul>
-    </div>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="logo.jpg" alt="">
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">Dispertaru</span>
+                    <span class="profession">DIY</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                <ul class="menu-links">
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bar-chart-alt-2 icon' ></i>
+                            <span class="text nav-text">Admin</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bell icon'></i>
+                            <span class="text nav-text">Tambah Dokumen</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-pie-chart-alt icon' ></i>
+                            <span class="text nav-text">Pengawasan</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon' ></i>
+                            <span class="text nav-text">Pemanfaatan</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-book icon' ></i>
+                            <span class="text nav-text">Read-Only</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-pie-chart-alt icon' ></i>
+                            <span class="text nav-text">Pengawasan</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon' ></i>
+                            <span class="text nav-text">Pemanfaatan</span>
+                        </a>
+                    </li>
+
+                    <li class="search-box">
+                        <i class='bx bx-search icon'></i>
+                        <input type="text" placeholder="Search...">
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+        <div class="text">Dashboard</div>
+    </section>
+
+    <script src="script.js"></script>
+
 </body>
 </html>
