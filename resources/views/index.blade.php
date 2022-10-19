b8854c4964049b90acd4b6b8ba7b3c72ac22694c
piisopii <shofiputried45@gmail.com>
2022-10-19 08:38:59 +0700

    nambah index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+    <div class="sidebar">
+        <header>Dinas Pertanahan dan Tata Ruang DIY</header>
+        <ul>
+            <li><a href="#"><i class="fas fa-user" style="color: black"></i></a></li>
+            <li><a href="#"><i class="fas fa-home" style="color: black"></i></a></li>
+            <li><a href="#"><i class="fas fa-folder" style="color: black"></i></a></li>
+            <li><a href="#"><i class="fas fa-qrcode" style="color: black"></i></a></li>
+            <li><a href="#"><i class="fas fa-document" style="color: black"></i></a></li>
+            <li><a href="#"><i class="fas fa-qrcode" style="color: black"></i></a></li>
+        </ul>
+    </div>
+</body>
+</html>
